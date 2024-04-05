0f73fdfb71c4c6bc4da2670f69de5b0e6f33b5a6
vishlv <vvishwakarma497@gmail.com>
2024-04-05 13:01:36 +0530

    added replacement of executable

--- utility/setupp.py
+++ utility/setupp.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.chrome.service import Service
 
 class Setup:
     def __init__(self, url):
@@ -10,7 +10,8 @@ class Setup:
         chrome_opt = Options()
         chrome_opt.binary_location = "D://WebDrivers//chrome-win64//chrome.exe"
         driver_path = "D://WebDrivers//chromedriver-win64//chromedriver.exe"
-        driver = webdriver.Chrome(executable_path=driver_path, options=chrome_opt)
+        service = Service(executable_path=driver_path)
+        driver = webdriver.Chrome(service=service, options=chrome_opt)
         driver.delete_all_cookies()
         driver.get(self.url)
         driver.set_page_load_timeout(3000)
